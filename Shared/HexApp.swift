3175e159e84840778830de973ff80734dc083473
Chris Ameter <cameter@gmail.com>
2021-02-19 11:23:51 -0700

    Initial Commit

---
+++ Shared/HexApp.swift
@@ -0,0 +1,17 @@
+//
+//  HexApp.swift
+//  Shared
+//
+//  Created by Chris on 2/19/21.
+//
+
+import SwiftUI
+
+@main
+struct HexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
