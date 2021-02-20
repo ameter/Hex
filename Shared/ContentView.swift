a66cdcd9e680c79249c41a7e703d93bdf0f0e483
Chris Ameter <cameter@gmail.com>
2021-02-20 13:21:13 -0700

    Made Hexagon an InsettableShape

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -7,57 +7,24 @@
 
 import SwiftUI
 
-struct Hexagon: Shape {
-    enum Orientation: Double {
-        case pointyTop
-        case flatTop
-    }
-    
-    let orientation: Orientation
-    
-    func path(in rect: CGRect) -> Path {
-        // We want to draw a hexagon
-        let sides = 6
-        let exteriorAngle = 360.0 / Double(sides) //* .pi / 180
-        let interiorAngle = Double(sides - 2) * 180.0 / Double(sides)
-        let rotation = orientation == .pointyTop ? 90.0 : interiorAngle / 2.0
-        
-        var size = 0.0
-        if orientation == .pointyTop {
-            let widthR = (Double(rect.width) / 2.0) / sin(exteriorAngle * .pi / 180)
-            let heightR = Double(rect.height) / 2.0
-            size = min(widthR, heightR)
-        } else {
-            let heightR = (Double(rect.height) / 2.0) / sin(exteriorAngle * .pi / 180)
-            let widthR = Double(rect.width) / 2.0
-            size = min(widthR, heightR)
-        }
-        
-        // we're ready to start with our path now
-        var path = Path()
-        
-        // loop over all our points
-        for side in 0...sides  {
-            let angle = (Double(side) * exteriorAngle - rotation) * .pi / 180
-            let point = CGPoint(
-                x: rect.midX + CGFloat(cos(angle) * size),
-                y: rect.midY + CGFloat(sin(angle) * size)
-            )
-            // move to our first corner or draw a line to our next corner
-            side == 0 ? path.move(to: point) : path.addLine(to: point)
-        }
-        path.closeSubpath()
-        
-        return path
-    }
-}
-
-
 struct ContentView: View {
     var body: some View {
         VStack {
+            Circle()
+                .inset(by: 5)
+            
             Hexagon(orientation: .pointyTop)
+//                .inset
+                .strokeBorder(lineWidth: 10.0)
+                .foregroundColor(.blue)
+                .onTapGesture {
+                    print("tapped")
+                }
                 .frame(width: 400, height: 250)
+                //.contentShape(Hexagon(orientation: .pointyTop))
+                
+                
+                
                 .background(Color.red)
         }
     }
---
+++ Shared/Hexagon.swift
@@ -0,0 +1,62 @@
+//
+//  Hexagon.swift
+//  Hex
+//
+//  Created by Chris on 2/20/21.
+//
+
+import SwiftUI
+
+struct Hexagon: InsettableShape {
+    var insetAmount: CGFloat = 0
+    
+    enum Orientation: Double {
+        case pointyTop
+        case flatTop
+    }
+    
+    let orientation: Orientation
+    
+    func path(in rect: CGRect) -> Path {
+        // We want to draw a hexagon
+        let sides = 6
+        let exteriorAngle = 360.0 / Double(sides) //* .pi / 180
+        let interiorAngle = Double(sides - 2) * 180.0 / Double(sides)
+        let rotation = orientation == .pointyTop ? 90.0 : interiorAngle / 2.0
+        let width = rect.width - insetAmount * 2
+        let height = rect.height - insetAmount * 2
+        var size = 0.0
+        if orientation == .pointyTop {
+            let widthR = (Double(width) / 2.0) / sin(exteriorAngle * .pi / 180)
+            let heightR = Double(height) / 2.0
+            size = min(widthR, heightR)
+        } else {
+            let heightR = (Double(height) / 2.0) / sin(exteriorAngle * .pi / 180)
+            let widthR = Double(width) / 2.0
+            size = min(widthR, heightR)
+        }
+        
+        // we're ready to start with our path now
+        var path = Path()
+        
+        // loop over all our points
+        for side in 0...sides  {
+            let angle = (Double(side) * exteriorAngle - rotation) * .pi / 180
+            let point = CGPoint(
+                x: rect.midX + CGFloat(cos(angle) * size),
+                y: rect.midY + CGFloat(sin(angle) * size)
+            )
+            // move to our first corner or draw a line to our next corner
+            side == 0 ? path.move(to: point) : path.addLine(to: point)
+        }
+        path.closeSubpath()
+        
+        return path
+    }
+    
+    func inset(by amount: CGFloat) -> some InsettableShape {
+        var hexagon = self
+        hexagon.insetAmount += amount
+        return hexagon
+    }
+}
